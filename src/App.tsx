1a36d9121f47b8e574c69a98f74c38a7ffd1a5c0
leejjeonghui <gdjh0513@naver.com>
2024-05-29 15:50:35 +0900

    초기화버튼(0이아닐때만 버튼보임)

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,23 @@ function App() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
   const setCount = 리턴값[1];
-  return(
-  <div>
-    <h1>{count}</h1>
-    <button onClick={()=>setCount((현재카운트)=>현재카운트+1)}>증가</button>
-    <button onClick={()=>setCount((현재카운트)=>현재카운트-1)}>감소</button>
-    <button onClick={()=>setCount((현재카운트)=>현재카운트=0)}>초기화</button>
-  </div>
-  
+  let 초기화;
+  if (count != 0) {
+    초기화 = <button onClick={() => setCount(0)}>초기화</button>;
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((현재카운트) => 현재카운트 + 1)}>
+        증가
+      </button>
+      <button onClick={() => setCount((현재카운트) => 현재카운트 - 1)}>
+        감소
+      </button>
+      {초기화}
+    </div>
   );
 }
-
 export default App;
+//{count===0?}
